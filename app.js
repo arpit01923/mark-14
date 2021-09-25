70cbe63ccee2e315ad87ec3b8dd15408535c9bf9
Arpit <arpitkumar01923@gmail.com>
2021-09-25 11:04:09 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var input1=document.querySelector("#i1");
+var input2=document.querySelector("#i2");
+var input3=document.querySelector("#i3");
+var btn=document.querySelector("#i4");
+var output=document.querySelector("#i5");
+ 
+function check(){
+    
+}
+
+btn.addEventListener("click",check);
